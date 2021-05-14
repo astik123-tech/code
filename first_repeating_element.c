548f2eb4352e44e4492b3d64fd35a0c5b66c7478
astik123-tech <69849255+astik123-tech@users.noreply.github.com>
2021-05-14 13:02:16 +0530

    Update first_repeating_element.c

--- first_repeating_element.c
+++ first_repeating_element.c
@@ -1,2 +1,5 @@
 // problem : given an array arr[]of size N . the task is to find the first repeating element in the array of integers,
 // i.e. an element that occurs more than once and without index of first occurrence is smallest.
+int main(){
+return 0;
+}
